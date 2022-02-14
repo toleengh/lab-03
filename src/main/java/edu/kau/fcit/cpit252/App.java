b20e5cb9315c7a01616f7f3d06ae060f86e330cf
Khalid Alharbi <kalharbi@users.noreply.github.com>
2022-02-14 23:16:49 +0300

    init commit

---
+++ src/main/java/edu/kau/fcit/cpit252/App.java
@@ -0,0 +1,8 @@
+package edu.kau.fcit.cpit252;
+
+public class App {
+    public static void main(String[] args) {
+        Car c = new Car("Honda", "Accord", 2019);
+        System.out.println(c);
+    }
+}
