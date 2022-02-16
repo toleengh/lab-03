09e65fbb0fdc8f42735c4940513525772b2dbe6e
Khalid Alharbi <kalharbi@users.noreply.github.com>
2022-02-16 10:57:47 +0300

    refactor main class

--- src/main/java/edu/kau/fcit/cpit252/App.java
+++ src/main/java/edu/kau/fcit/cpit252/App.java
@@ -2,8 +2,10 @@ package edu.kau.fcit.cpit252;
 
 public class App {
     public static void main(String[] args) {
-        Car c = new Car("Honda", "Accord", 2019);
-        System.out.println(c);
+        Car c1 = new Car("Honda", "Accord", 2019);
+        // print car and recall info
+        System.out.println(c1);
+        // create a duplicate car object to print the recall info in a table
         Car c2 = new Car("Honda", "Accord", 2019);
         RecallViewer rv = new RecallViewer(c2);
         rv.printTableView();
