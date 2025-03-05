36434317fd0aec7ba126f59529fb948d4ab31893
toleengh <117521768+toleengh@users.noreply.github.com>
2025-03-06 00:51:44 +0300

    Update App.java

--- src/main/java/edu/kau/fcit/cpit252/App.java
+++ src/main/java/edu/kau/fcit/cpit252/App.java
@@ -3,10 +3,10 @@ package edu.kau.fcit.cpit252;
 public class App {
     public static void main(String[] args) {
         Car c1 = new Car("Honda", "Accord", 2019);
-        // print car and recall info
         System.out.println(c1);
-        // create a duplicate car object to print the recall info in a table
-        Car c2 = new Car("Honda", "Accord", 2019);
+
+        // Clone instead of creating a new Car object
+        Car c2 = c1.clone();
         RecallViewer rv = new RecallViewer(c2);
         rv.printTableView();
     }
