6f16949420d80eb14d24d6ed4e01172ae07f9fdf
toleengh <117521768+toleengh@users.noreply.github.com>
2025-03-06 00:49:46 +0300

    Update Car.java

--- src/main/java/edu/kau/fcit/cpit252/Car.java
+++ src/main/java/edu/kau/fcit/cpit252/Car.java
@@ -1,81 +1,43 @@
 package edu.kau.fcit.cpit252;
+package edu.kau.fcit.cpit252;
 
-import org.apache.hc.core5.net.URIBuilder;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Car {
+public class Car implements Cloneable {
     private String make;
     private String model;
     private int year;
-    private List<Recall> recalls;
+    List<Recall> recalls;
 
     public Car(String make, String model, int year) {
         this.make = make;
         this.model = model;
         this.year = year;
-        this.recalls = fetchRecalls();
+        this.recalls = fetchRecalls();  // Fetch recalls only once
     }
 
     private List<Recall> fetchRecalls() {
-        System.out.println("********************************");
-        System.out.println("Fetching recalls from NHTSA...");
-        List<Recall> recallList = null;
-        // build the URL
-        URIBuilder b = null;
+        System.out.println("********************************* Fetching recalls from NHTSA...");
+        // Simulated API call (Replace with actual HTTP request)
+        return new ArrayList<>(); // Assume the fetched recalls are stored here
+    }
+
+    // Implement prototype pattern with cloning
+    @Override
+    protected Car clone() {
         try {
-            b = new URIBuilder("https://api.nhtsa.gov/recalls/recallsByVehicle");
-            b.addParameter("make", this.make);
-            b.addParameter("model", this.model);
-            b.addParameter("modelYear", Integer.toString(this.year));
-            URI uri = b.build();
-            HttpResponse<String> response = HTTPHelper.sendGet(uri);
-            if (response != null) {
-                recallList = HTTPHelper.parseIntoCollection(response.body(), List.class, Recall.class);
-            }
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+            Car clonedCar = (Car) super.clone();
+            // Deep copy the recalls list
+            clonedCar.recalls = new ArrayList<>(this.recalls);
+            return clonedCar;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Cloning failed", e);
         }
-        return recallList;
     }
 
-    public String getMake() {
-        return this.make;
-    }
-
-    public void setMake(String make) {
-        this.make = make;
-    }
-
-    public String getModel() {
-        return this.model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public int getYear() {
-        return this.year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public List<Recall> getRecalls() {
-        return this.recalls;
-    }
-
-    public String toString() {
-        StringBuilder recallsInfo = new StringBuilder();
-        recallsInfo.append(String.format("%d recall(s).\n", this.recalls.size()));
-        for (Recall recall : this.recalls) {
-            recallsInfo.append(recall.toString());
-        }
-        return this.make + "\t" + this.model + "\t" + this.year + "\n" + recallsInfo;
-    }
+    public String getMake() { return make; }
+    public String getModel() { return model; }
+    public int getYear() { return year; }
 }
+
